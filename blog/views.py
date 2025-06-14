6c4fbff1c37a9dbf9752c3e93f7f1ada22c0e668
morCeus <mohammadwkwk@gmail.com>
2025-06-14 22:29:23 +0800

    Add html templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
+
+#def post_list(request):
+    #return HttpResponse("<h1>Hello my Mors!</h1>")
